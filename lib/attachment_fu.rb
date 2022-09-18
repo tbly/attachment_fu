497b089bd4e65b24c7aee248ff4e519466c826c6
Ly Tieu Bang <lytieubang@yahoo.com>
2022-09-18 20:25:01 +0800

    test initializer gem

--- lib/attachment_fu.rb
+++ lib/attachment_fu.rb
@@ -14,6 +14,13 @@ if defined?(Rails::Engine)
       Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
       FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
     end
+
+    puts "TEST INIT GEM HERE 2"
+    require 'geometry'
+
+    ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
+    Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
+    FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
   end
 else
   # Rails <= 2
