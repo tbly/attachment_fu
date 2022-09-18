6b63ecd1f23048f07f4921d1123bbab0eb80d836
Ly Tieu Bang <lytieubang@yahoo.com>
2022-09-18 20:37:30 +0800

    test initializer gem

--- lib/attachment_fu.rb
+++ lib/attachment_fu.rb
@@ -6,21 +6,22 @@ if defined?(Rails::Engine)
     # Mimic old vendored plugin behavior, attachment_fu/lib is autoloaded.
     config.autoload_paths << File.expand_path("..", __FILE__)
 
-    initializer "attachment_fu" do
-      puts "TEST INIT GEM HERE"
+    initializer "mini_attachment_fu" do
+      puts "TEST INIT GEM HERE 1"
       require 'geometry'
 
       ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
       Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
       FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
     end
-
+    if false
     puts "TEST INIT GEM HERE 2"
     require 'geometry'
 
     ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
     Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
     FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
+  end
   end
 else
   # Rails <= 2
